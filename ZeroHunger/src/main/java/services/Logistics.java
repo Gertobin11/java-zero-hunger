0eb0b93903c73ab76034416b17ae8b16c3674958
Gerard Tobin <gertobin11@gmail.com>
2025-07-15 22:27:06 +0100

    added track delivery method

--- ZeroHunger/src/main/java/services/Logistics.java
+++ ZeroHunger/src/main/java/services/Logistics.java
@@ -6,9 +6,12 @@ package services;
 
 import grpc.logistics.Delivery;
 import grpc.logistics.DeliveryResponse;
+import grpc.logistics.Location;
+import grpc.logistics.LocationUpdate;
 import grpc.logistics.LogisticsServiceGrpc.LogisticsServiceImplBase;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import static io.grpc.Status.NOT_FOUND;
 import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -45,7 +48,7 @@ public class Logistics extends LogisticsServiceImplBase {
      * Method that accepts and rejects requests for delivery
      *
      * @param delivery the items to deliver, the pickup address and the
-     * destination       
+     * destination
      * @param responseObserver the response we send to the client
      */
     @Override
@@ -71,6 +74,92 @@ public class Logistics extends LogisticsServiceImplBase {
 
         // tell the client the method is finished
         responseObserver.onCompleted();
+    }
+
+    /**
+     * Method that accepts a stream of location update requests and gives checks
+     * to see if has been delivered, and responds with location update
+     *
+     * @param responseObserver used to send a response to the client
+     * @return current latitude and longitude and the current status of the
+     * delivery
+     */
+    @Override
+    public StreamObserver<LocationUpdate> trackDelivery(StreamObserver<Location> responseObserver) {
+
+        System.out.println("Streaming delivery location....");
+        // we just check if a delivery has been accepted then we simulate the latitude longitude moving towards a delivery
+        return new StreamObserver<LocationUpdate>() {
+            // mock location as int -- Tralee town center
+            // this would be sent via smart devices on the truck or van
+            int currentLongitude = 522709;
+            int currentLatitude = -96974;
+
+            //  mock location as int for destination
+            // this would be generated from the eircode
+            int destinationLongitude = 532709;
+            int destinationLatitude = -97974;
+
+            @Override
+            public void onNext(LocationUpdate value) {
+                // we set the status of the current delivery
+                String status = "Enroute";
+                // we move the delivery towards the destination
+                if (currentLongitude < destinationLongitude) {
+                    currentLongitude++;
+                }
+
+                if (currentLatitude > destinationLatitude) {
+                    currentLatitude--;
+                }
+
+                // We check if the delivery is at the destination, then change the status
+                if (currentLongitude == destinationLongitude && currentLatitude == destinationLatitude) {
+                    status = "Delivered";
+                }
+
+                // every on next we verify that the request is for a valid delivery
+                if (!acceptedDeliveries.contains(value.getDeliveryId())) {
+                    responseObserver.onError(NOT_FOUND.
+                            withDescription("Delivery Request with an ID of " + value.
+                                    getDeliveryId() + " not found").
+                            asException());
+                } else {
+                    // we change the longitude to the proper string format
+                    String longitude = (currentLongitude + "");
+                    String formattedLongitude = longitude.substring(0, 2) + "." + longitude.
+                            substring(2);
+                    // we change the latitude to the proper string format
+                    String latitude = (currentLatitude + "");
+                    String formattedLatitude = latitude.substring(0, 2) + "." + latitude.
+                            substring(2);
+
+                    // we build the current location message
+                    Location location = Location.newBuilder().
+                            setDeliveryId(value.getDeliveryId()).
+                            setLatitude(formattedLatitude).
+                            setLongitude(formattedLongitude).
+                            setStatus(status).
+                            build();
+
+                    // we stream the current location to the caller
+                    responseObserver.onNext(location);
+
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Request for delivery location update ended");
+                responseObserver.onCompleted();
+            }
+        };
+
     }
 
 }
