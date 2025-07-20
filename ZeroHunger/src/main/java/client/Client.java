1f7d1db43004548feac72ffa2e428732a6bd49a2
Gerard Tobin <gertobin11@gmail.com>
2025-07-20 23:09:26 +0100

    add basic ui boilerplate

--- ZeroHunger/src/main/java/client/Client.java
+++ ZeroHunger/src/main/java/client/Client.java
@@ -15,7 +15,18 @@ import grpc.smart_hub.SmartHubServiceGrpc;
 import grpc.smart_hub.StatusResponse;
 import io.grpc.ManagedChannel;
 import io.grpc.StatusRuntimeException;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.io.IOException;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
 
 /**
  *
@@ -24,6 +35,24 @@ import java.io.IOException;
 public class Client {
 
     public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            JFrame parentFrame = new JFrame();
+            // how to make a Jframe fullscreen https://stackoverflow.com/questions/11570356/jframe-in-full-screen-java
+            parentFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            parentFrame.setUndecorated(true);
+            parentFrame.setVisible(true);
+
+            JPanel mainContainer = new JPanel(new BorderLayout(10, 10));
+            mainContainer.setBorder(new EmptyBorder(10, 10, 10, 10));
+            mainContainer.setBackground(new Color(240, 240, 240));
+            JLabel mainHeading = new JLabel("Zero Hunger - Smart Food Management Service", SwingConstants.CENTER);
+            parentFrame.add(mainContainer);
+            mainContainer.add(mainHeading);
+
+            parentFrame.setVisible(true);
+        });
+
         try {
             // on loading, trigger a system check
             ServiceDiscovery serviceDiscovery = new ServiceDiscovery();
@@ -40,15 +69,28 @@ public class Client {
             smartHub = SmartHubServiceGrpc.newBlockingStub(smartHubChannel);
             Empty emptyRequest = Empty.newBuilder().
                     build();
-            Address address = Address.newBuilder().setAddress("test").setEircode("v92 1234").build();
-            FoodItem item = FoodItem.newBuilder().setId(1).setName("Milk").build();
-            FoodItemQuantity itemQuantity = FoodItemQuantity.newBuilder().setItem(item).setQuantity(1).build();
-            FoodRequest request = FoodRequest.newBuilder().addItems(itemQuantity).setDestination(address).build();
+            Address address = Address.newBuilder().
+                    setAddress("test").
+                    setEircode("v92 1234").
+                    build();
+            FoodItem item = FoodItem.newBuilder().
+                    setId(1).
+                    setName("Milk").
+                    build();
+            FoodItemQuantity itemQuantity = FoodItemQuantity.newBuilder().
+                    setItem(item).
+                    setQuantity(1).
+                    build();
+            FoodRequest request = FoodRequest.newBuilder().
+                    addItems(itemQuantity).
+                    setDestination(address).
+                    build();
             StatusResponse response = smartHub.handleFoodRequests(request);
             System.out.println(response.getStatus());
             smartHub.triggerChecks(emptyRequest);
         } catch (IOException | StatusRuntimeException ex) {
-            System.out.println("Error when running the client. Error: " + ex.getMessage());
+            System.out.println("Error when running the client. Error: " + ex.
+                    getMessage());
         }
 
     }
